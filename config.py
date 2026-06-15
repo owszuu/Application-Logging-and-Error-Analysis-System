2ce046fcc85aa9bf6f19b9bc665035886bb22e97
naciorrozyc2004 <naciorrozyc@gmail.com>
2026-06-15 15:53:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 
-# Wklej tutaj swój connection string z MongoDB Atlas
 CONNECTION_STRING = "mongodb+srv://zuzswo_db_user:zbazowane@cluster0.vlex7cc.mongodb.net/?appName=Cluster0"
 
 # Nazwa bazy danych
